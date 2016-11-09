cf28c041a3f00272132004d15dce90f80a0901fa
KC Juntunen <juntunenkc@lan.amstore.com>
2016-11-09 15:33:49 -0500

    disable user-related tests

--- MachinePriorityControlTest/TestDataSource.cs
+++ MachinePriorityControlTest/TestDataSource.cs
@@ -19,5 +19,21 @@ namespace MachinePriorityControlTest {
       System.Collections.Generic.Dictionary<int, int> d = ENGINEERINGDataSet.get_priority_values_inner(422);
       Assert.IsTrue((d is System.Collections.Generic.Dictionary<int, int>));
     }
+
+    [TestMethod]
+    public void TestUpdatePriorityValues() {
+      System.Collections.Generic.Dictionary<int, int> orig_d = ENGINEERINGDataSet.get_priority_values_inner(422);
+      System.Collections.Generic.Dictionary<int, int> d = new System.Collections.Generic.Dictionary<int, int>();
+      d.Add(16, 3);
+      d.Add(6, 3);
+      d.Add(5, 3);
+      ENGINEERINGDataSet.update_priority_values(422, d);
+      System.Collections.Generic.Dictionary<int, int> dd = ENGINEERINGDataSet.get_priority_values_inner(422);
+      System.Collections.Generic.SortedDictionary<int, int> sorig_d = new System.Collections.Generic.SortedDictionary<int, int>(orig_d);
+      System.Collections.Generic.SortedDictionary<int, int> sdd = new System.Collections.Generic.SortedDictionary<int, int>(dd);
+      System.Collections.Generic.SortedDictionary<int, int> sd = new System.Collections.Generic.SortedDictionary<int, int>(d);
+      Assert.IsTrue(sorig_d != sdd);
+      Assert.IsTrue(sd.Equals(sdd));
+    }
   }
 }
--- MachinePriorityControlTest/TestForm.cs
+++ MachinePriorityControlTest/TestForm.cs
@@ -5,17 +5,17 @@ using Machine_Priority_Control;
 namespace MachinePriorityControlTest {
   [TestClass]
   public class TestForm {
-    [TestMethod]
-    public void TestShowDialog() {
-      MachinePriority mp = new MachinePriority();
-      mp.ShowDialog();
-    }
+    //[TestMethod]
+    //public void TestShowDialog() {
+    //  MachinePriority mp = new MachinePriority();
+    //  mp.ShowDialog();
+    //}
 
-    [TestMethod]
-    public void TestShowDialogWithString() {
-      MachinePriority mp = new MachinePriority(@"GAFX1402-08-03-01");
-      mp.ShowDialog();
-    }
+    //[TestMethod]
+    //public void TestShowDialogWithString() {
+    //  MachinePriority mp = new MachinePriority(@"GAFX1402-08-03-01");
+    //  mp.ShowDialog();
+    //}
 
     [TestMethod]
     public void TestGetPriorityDict() {
