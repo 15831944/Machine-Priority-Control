b43eff42edab0bb22c5c95a77f7e5a2dbe7363da
KC Juntunen <juntunenkc@lan.amstore.com>
2016-11-29 10:25:00 -0500

    Adjusted layout table for top row of controls

--- Machine Priority Control/MachinePriority.Designer.cs
+++ Machine Priority Control/MachinePriority.Designer.cs
@@ -91,7 +91,7 @@
       this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
       this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
       this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-      this.tableLayoutPanel1.Size = new System.Drawing.Size(728, 393);
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(758, 393);
       this.tableLayoutPanel1.TabIndex = 0;
       // 
       // tableLayoutPanel2
@@ -110,7 +110,7 @@
       this.tableLayoutPanel2.Name = "tableLayoutPanel2";
       this.tableLayoutPanel2.RowCount = 1;
       this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-      this.tableLayoutPanel2.Size = new System.Drawing.Size(722, 29);
+      this.tableLayoutPanel2.Size = new System.Drawing.Size(752, 29);
       this.tableLayoutPanel2.TabIndex = 1;
       // 
       // buttonCancel
@@ -118,7 +118,7 @@
       this.buttonCancel.Dock = System.Windows.Forms.DockStyle.Fill;
       this.buttonCancel.Location = new System.Drawing.Point(3, 3);
       this.buttonCancel.Name = "buttonCancel";
-      this.buttonCancel.Size = new System.Drawing.Size(234, 23);
+      this.buttonCancel.Size = new System.Drawing.Size(244, 23);
       this.buttonCancel.TabIndex = 14;
       this.buttonCancel.Text = "Cancel";
       this.buttonCancel.UseVisualStyleBackColor = true;
@@ -127,9 +127,9 @@
       // buttonOK
       // 
       this.buttonOK.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.buttonOK.Location = new System.Drawing.Point(483, 3);
+      this.buttonOK.Location = new System.Drawing.Point(503, 3);
       this.buttonOK.Name = "buttonOK";
-      this.buttonOK.Size = new System.Drawing.Size(236, 23);
+      this.buttonOK.Size = new System.Drawing.Size(246, 23);
       this.buttonOK.TabIndex = 15;
       this.buttonOK.Text = "OK";
       this.buttonOK.UseVisualStyleBackColor = true;
@@ -138,9 +138,9 @@
       // buttonApply
       // 
       this.buttonApply.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.buttonApply.Location = new System.Drawing.Point(243, 3);
+      this.buttonApply.Location = new System.Drawing.Point(253, 3);
       this.buttonApply.Name = "buttonApply";
-      this.buttonApply.Size = new System.Drawing.Size(234, 23);
+      this.buttonApply.Size = new System.Drawing.Size(244, 23);
       this.buttonApply.TabIndex = 16;
       this.buttonApply.Text = "Apply";
       this.buttonApply.UseVisualStyleBackColor = true;
@@ -172,7 +172,7 @@
       this.tableLayoutPanel3.RowCount = 2;
       this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
       this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-      this.tableLayoutPanel3.Size = new System.Drawing.Size(722, 314);
+      this.tableLayoutPanel3.Size = new System.Drawing.Size(752, 314);
       this.tableLayoutPanel3.TabIndex = 0;
       // 
       // listBox4
@@ -185,12 +185,13 @@
       this.listBox4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.listBox4.FormattingEnabled = true;
       this.listBox4.ItemHeight = 17;
-      this.listBox4.Location = new System.Drawing.Point(147, 38);
+      this.listBox4.Location = new System.Drawing.Point(153, 38);
       this.listBox4.Name = "listBox4";
       this.listBox4.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-      this.listBox4.Size = new System.Drawing.Size(138, 259);
+      this.listBox4.Size = new System.Drawing.Size(144, 259);
       this.listBox4.TabIndex = 15;
       this.listBox4.ValueMember = "MACHID";
+      this.listBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox4_MouseClick);
       // 
       // cUTMACHINESBindingSource3
       // 
@@ -208,7 +209,7 @@
       this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
       this.label7.Location = new System.Drawing.Point(3, 0);
       this.label7.Name = "label7";
-      this.label7.Size = new System.Drawing.Size(138, 35);
+      this.label7.Size = new System.Drawing.Size(144, 35);
       this.label7.TabIndex = 6;
       this.label7.Text = "Common Parts";
       this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -222,7 +223,7 @@
       this.listBox5.FormattingEnabled = true;
       this.listBox5.Location = new System.Drawing.Point(3, 38);
       this.listBox5.Name = "listBox5";
-      this.listBox5.Size = new System.Drawing.Size(138, 264);
+      this.listBox5.Size = new System.Drawing.Size(144, 264);
       this.listBox5.TabIndex = 7;
       this.listBox5.ValueMember = "PARTID";
       this.listBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox5_MouseClick);
@@ -233,9 +234,9 @@
       this.label3.AutoSize = true;
       this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
       this.label3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label3.Location = new System.Drawing.Point(579, 0);
+      this.label3.Location = new System.Drawing.Point(603, 0);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(140, 35);
+      this.label3.Size = new System.Drawing.Size(146, 35);
       this.label3.TabIndex = 12;
       this.label3.Text = "3";
       this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -245,9 +246,9 @@
       this.label2.AutoSize = true;
       this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
       this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label2.Location = new System.Drawing.Point(435, 0);
+      this.label2.Location = new System.Drawing.Point(453, 0);
       this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(138, 35);
+      this.label2.Size = new System.Drawing.Size(144, 35);
       this.label2.TabIndex = 10;
       this.label2.Text = "2";
       this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -257,9 +258,9 @@
       this.label1.AutoSize = true;
       this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
       this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label1.Location = new System.Drawing.Point(291, 0);
+      this.label1.Location = new System.Drawing.Point(303, 0);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(138, 35);
+      this.label1.Size = new System.Drawing.Size(144, 35);
       this.label1.TabIndex = 8;
       this.label1.Text = "1";
       this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -274,10 +275,10 @@
       this.listBox3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.listBox3.FormattingEnabled = true;
       this.listBox3.ItemHeight = 17;
-      this.listBox3.Location = new System.Drawing.Point(579, 38);
+      this.listBox3.Location = new System.Drawing.Point(603, 38);
       this.listBox3.Name = "listBox3";
       this.listBox3.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-      this.listBox3.Size = new System.Drawing.Size(140, 259);
+      this.listBox3.Size = new System.Drawing.Size(146, 259);
       this.listBox3.TabIndex = 13;
       this.listBox3.ValueMember = "MACHID";
       this.listBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox3_MouseClick);
@@ -297,10 +298,10 @@
       this.listBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.listBox2.FormattingEnabled = true;
       this.listBox2.ItemHeight = 17;
-      this.listBox2.Location = new System.Drawing.Point(435, 38);
+      this.listBox2.Location = new System.Drawing.Point(453, 38);
       this.listBox2.Name = "listBox2";
       this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-      this.listBox2.Size = new System.Drawing.Size(138, 259);
+      this.listBox2.Size = new System.Drawing.Size(144, 259);
       this.listBox2.TabIndex = 11;
       this.listBox2.ValueMember = "MACHID";
       this.listBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseClick);
@@ -320,10 +321,10 @@
       this.listBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.listBox1.FormattingEnabled = true;
       this.listBox1.ItemHeight = 17;
-      this.listBox1.Location = new System.Drawing.Point(291, 38);
+      this.listBox1.Location = new System.Drawing.Point(303, 38);
       this.listBox1.Name = "listBox1";
       this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-      this.listBox1.Size = new System.Drawing.Size(138, 259);
+      this.listBox1.Size = new System.Drawing.Size(144, 259);
       this.listBox1.TabIndex = 9;
       this.listBox1.ValueMember = "MACHID";
       this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
@@ -338,9 +339,9 @@
       this.label8.AutoSize = true;
       this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
       this.label8.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label8.Location = new System.Drawing.Point(147, 0);
+      this.label8.Location = new System.Drawing.Point(153, 0);
       this.label8.Name = "label8";
-      this.label8.Size = new System.Drawing.Size(138, 35);
+      this.label8.Size = new System.Drawing.Size(144, 35);
       this.label8.TabIndex = 14;
       this.label8.Text = "0";
       this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -350,12 +351,12 @@
       this.tableLayoutPanel4.Anchor = System.Windows.Forms.AnchorStyles.Left;
       this.tableLayoutPanel4.ColumnCount = 7;
       this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
       this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
       this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
       this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
       this.tableLayoutPanel4.Controls.Add(this.label4, 0, 0);
       this.tableLayoutPanel4.Controls.Add(this.comboBox1, 1, 0);
       this.tableLayoutPanel4.Controls.Add(this.textBox2, 6, 0);
@@ -367,7 +368,7 @@
       this.tableLayoutPanel4.Name = "tableLayoutPanel4";
       this.tableLayoutPanel4.RowCount = 1;
       this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-      this.tableLayoutPanel4.Size = new System.Drawing.Size(598, 32);
+      this.tableLayoutPanel4.Size = new System.Drawing.Size(752, 32);
       this.tableLayoutPanel4.TabIndex = 2;
       // 
       // label4
@@ -392,7 +393,7 @@
       this.comboBox1.FormattingEnabled = true;
       this.comboBox1.Location = new System.Drawing.Point(53, 3);
       this.comboBox1.Name = "comboBox1";
-      this.comboBox1.Size = new System.Drawing.Size(142, 21);
+      this.comboBox1.Size = new System.Drawing.Size(161, 21);
       this.comboBox1.TabIndex = 1;
       this.comboBox1.ValueMember = "PARTID";
       this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -410,15 +411,15 @@
       this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
       this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.cUTPARTSBindingSource, "CNC2", true));
       this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Tag", this.cUTPARTSBindingSource, "PARTNUM", true));
-      this.textBox2.Location = new System.Drawing.Point(501, 3);
+      this.textBox2.Location = new System.Drawing.Point(587, 3);
       this.textBox2.Name = "textBox2";
-      this.textBox2.Size = new System.Drawing.Size(94, 15);
+      this.textBox2.Size = new System.Drawing.Size(162, 15);
       this.textBox2.TabIndex = 5;
       // 
       // label6
       // 
       this.label6.AutoSize = true;
-      this.label6.Location = new System.Drawing.Point(451, 0);
+      this.label6.Location = new System.Drawing.Point(537, 0);
       this.label6.Name = "label6";
       this.label6.Size = new System.Drawing.Size(38, 13);
       this.label6.TabIndex = 4;
@@ -432,15 +433,15 @@
       this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
       this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.cUTPARTSBindingSource, "CNC1", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
       this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Tag", this.cUTPARTSBindingSource, "PARTNUM", true));
-      this.textBox1.Location = new System.Drawing.Point(351, 3);
+      this.textBox1.Location = new System.Drawing.Point(370, 3);
       this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(94, 15);
+      this.textBox1.Size = new System.Drawing.Size(161, 15);
       this.textBox1.TabIndex = 3;
       // 
       // label5
       // 
       this.label5.AutoSize = true;
-      this.label5.Location = new System.Drawing.Point(301, 0);
+      this.label5.Location = new System.Drawing.Point(320, 0);
       this.label5.Name = "label5";
       this.label5.Size = new System.Drawing.Size(38, 13);
       this.label5.TabIndex = 2;
@@ -452,7 +453,7 @@
       this.checkBox1.AutoSize = true;
       this.checkBox1.Checked = true;
       this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-      this.checkBox1.Location = new System.Drawing.Point(201, 3);
+      this.checkBox1.Location = new System.Drawing.Point(220, 3);
       this.checkBox1.Name = "checkBox1";
       this.checkBox1.Size = new System.Drawing.Size(52, 17);
       this.checkBox1.TabIndex = 6;
@@ -481,7 +482,7 @@
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(752, 417);
+      this.ClientSize = new System.Drawing.Size(782, 417);
       this.Controls.Add(this.tableLayoutPanel1);
       this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.Name = "MachinePriority";
