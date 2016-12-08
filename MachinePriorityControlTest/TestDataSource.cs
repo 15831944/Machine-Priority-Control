e55e98cea8220e8ae7b3da7e8e552430dece670c
KC Juntunen <juntunenkc@lan.amstore.com>
2016-12-08 13:48:20 -0500

    I shouldn't mess with Dan's part data

--- MachinePriorityControlTest/TestDataSource.cs
+++ MachinePriorityControlTest/TestDataSource.cs
@@ -21,22 +21,22 @@ namespace MachinePriorityControlTest {
       Assert.IsTrue((d is System.Collections.Generic.Dictionary<int, int>));
     }
 
-    [TestMethod]
-    public void TestUpdatePriorityValues() {
-      System.Collections.Generic.Dictionary<int, int> orig_d = ENGINEERINGDataSet.get_priority_values_inner(422);
-      System.Collections.Generic.Dictionary<int, int> d = ENGINEERINGDataSet.get_priority_values_inner(422);
-      d[16] = 3;
-      d[6] = 3;
-      d[5] = 3;
-      ENGINEERINGDataSet.update_priority_values(422, d);
-      System.Collections.Generic.Dictionary<int, int> dd = ENGINEERINGDataSet.get_priority_values_inner(422);
-      System.Collections.Generic.SortedDictionary<int, int> sorig_d = new System.Collections.Generic.SortedDictionary<int, int>(orig_d);
-      System.Collections.Generic.SortedDictionary<int, int> sdd = new System.Collections.Generic.SortedDictionary<int, int>(dd);
-      System.Collections.Generic.SortedDictionary<int, int> sd = new System.Collections.Generic.SortedDictionary<int, int>(d);
-      var dict = sorig_d.Where(entry => sdd[entry.Key] != entry.Value).ToDictionary(entry => entry.Key, entry => entry.Value);
-      Assert.IsTrue(dict.Count > 0);
-      dict = sdd.Where(entry => sd[entry.Key] != entry.Value).ToDictionary(entry => entry.Key, entry => entry.Value);
-      Assert.IsTrue(dict.Count == 0);
-    }
+    //[TestMethod]
+    //public void TestUpdatePriorityValues() {
+    //  System.Collections.Generic.Dictionary<int, int> orig_d = ENGINEERINGDataSet.get_priority_values_inner(422);
+    //  System.Collections.Generic.Dictionary<int, int> d = ENGINEERINGDataSet.get_priority_values_inner(422);
+    //  d[16] = 3;
+    //  d[6] = 3;
+    //  d[5] = 3;
+    //  ENGINEERINGDataSet.update_priority_values(422, d);
+    //  System.Collections.Generic.Dictionary<int, int> dd = ENGINEERINGDataSet.get_priority_values_inner(422);
+    //  System.Collections.Generic.SortedDictionary<int, int> sorig_d = new System.Collections.Generic.SortedDictionary<int, int>(orig_d);
+    //  System.Collections.Generic.SortedDictionary<int, int> sdd = new System.Collections.Generic.SortedDictionary<int, int>(dd);
+    //  System.Collections.Generic.SortedDictionary<int, int> sd = new System.Collections.Generic.SortedDictionary<int, int>(d);
+    //  var dict = sorig_d.Where(entry => sdd[entry.Key] != entry.Value).ToDictionary(entry => entry.Key, entry => entry.Value);
+    //  Assert.IsTrue(dict.Count > 0);
+    //  dict = sdd.Where(entry => sd[entry.Key] != entry.Value).ToDictionary(entry => entry.Key, entry => entry.Value);
+    //  Assert.IsTrue(dict.Count == 0);
+    //}
   }
 }
